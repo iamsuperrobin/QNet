80d167af00eb104e25ad0b1ecdf069f2023991c9
Robin <Robin@PC-ROBIN>
2018-02-09 15:17:08 +0000

    Simple neural network

---
+++ QNet.cpp
@@ -0,0 +1,403 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cassert>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+// Silly class to read training data from a text file -- Replace This.
+// Replace class TrainingData with whatever you need to get input data into the
+// program, e.g., connect to a database, or take a stream of data from stdin, or
+// from a file specified by a command line argument, etc.
+
+class TrainingData
+{
+public:
+	TrainingData(const string filename);
+	bool isEof(void) { return m_trainingDataFile.eof(); }
+	void getTopology(vector<unsigned> &topology);
+
+	// Returns the number of input values read from the file:
+	unsigned getNextInputs(vector<double> &inputVals);
+	unsigned getTargetOutputs(vector<double> &targetOutputVals);
+
+private:
+	ifstream m_trainingDataFile;
+};
+
+void TrainingData::getTopology(vector<unsigned> &topology)
+{
+	string line;
+	string label;
+
+	getline(m_trainingDataFile, line);
+	stringstream ss(line);
+	ss >> label;
+	if (this->isEof() || label.compare("topology:") != 0) {
+		abort();
+	}
+
+	while (!ss.eof()) {
+		unsigned n;
+		ss >> n;
+		topology.push_back(n);
+	}
+
+	return;
+}
+
+TrainingData::TrainingData(const string filename)
+{
+	m_trainingDataFile.open(filename.c_str());
+}
+
+unsigned TrainingData::getNextInputs(vector<double> &inputVals)
+{
+	inputVals.clear();
+
+	string line;
+	getline(m_trainingDataFile, line);
+	stringstream ss(line);
+
+	string label;
+	ss >> label;
+	if (label.compare("in:") == 0) {
+		double oneValue;
+		while (ss >> oneValue) {
+			inputVals.push_back(oneValue);
+		}
+	}
+
+	return inputVals.size();
+}
+
+unsigned TrainingData::getTargetOutputs(vector<double> &targetOutputVals)
+{
+	targetOutputVals.clear();
+
+	string line;
+	getline(m_trainingDataFile, line);
+	stringstream ss(line);
+
+	string label;
+	ss >> label;
+	if (label.compare("out:") == 0) {
+		double oneValue;
+		while (ss >> oneValue) {
+			targetOutputVals.push_back(oneValue);
+		}
+	}
+
+	return targetOutputVals.size();
+}
+
+class Neuron;
+typedef vector<Neuron> Layer;
+
+struct Connection
+{
+	double weight;
+	double deltaWeight;
+};
+
+class Neuron
+{
+public:
+	Neuron(unsigned numOutputs, unsigned myIndex);
+	void setOutputVal(double val) { m_outputVal = val; }
+	double getOutputVal(void) const { return m_outputVal; }
+	void feedForward(const Layer &prevLayer);
+	void calcOutputGradients(double targetVal);
+	void calcHiddenGradients(const Layer &nextLayer);
+	void updateInputWeights(Layer &prevLayer);
+private:
+	static double eta; // [0.0 - 1.0] overall net training rate
+	static double alpha; // [0.0 - 1.0] multiplier of last weight change (momentum)
+	static double transferFunction(double x);
+	static double transferFunctionDerivative(double x);
+	static double randomWeight(void) { return rand() / double(RAND_MAX); }
+	double sumDOW(const Layer &nextLayer);
+	double m_outputVal;
+	vector<Connection> m_outputWeights;
+	unsigned m_myIndex;
+	double m_gradient;
+};
+
+Neuron::Neuron(unsigned numOutputs, unsigned myIndex)
+{
+	for (unsigned c = 0; c < numOutputs; ++c)
+	{
+		m_outputWeights.push_back(Connection());
+		m_outputWeights.back().weight = randomWeight();
+	}
+
+	m_myIndex = myIndex;
+}
+
+double Neuron::eta = 0.15;
+double Neuron::alpha = 0.5;
+
+
+double Neuron::transferFunction(double x)
+{
+	return tanh(x);
+}
+
+double Neuron::transferFunctionDerivative(double x)
+{
+	return 1.0 - x*x;
+}
+
+double Neuron::sumDOW(const Layer &nextLayer)
+{
+	double sum = 0.0;
+
+	//sum out contributions of the errors at the nodes we feed
+
+	for (unsigned n = 0; n < nextLayer.size() - 1; ++n)
+	{
+		sum += m_outputWeights[n].weight * nextLayer[n].m_gradient;
+	}
+
+	return sum;
+}
+
+void Neuron::updateInputWeights(Layer &prevLayer)
+{
+	// The weights to be updated are in the connection container
+	// in the neurons in the preceding layer
+
+	for (unsigned n = 0; n < prevLayer.size(); ++n)
+	{
+		Neuron &neuron = prevLayer[n];
+		double oldDeltaWeight = neuron.m_outputWeights[m_myIndex].deltaWeight;
+
+		double newDeltaWeight = eta * neuron.getOutputVal() * m_gradient + alpha * oldDeltaWeight;
+
+		neuron.m_outputWeights[m_myIndex].deltaWeight = newDeltaWeight;
+		neuron.m_outputWeights[m_myIndex].weight += newDeltaWeight;
+	}
+}
+
+
+void Neuron::feedForward(const Layer &prevLayer)
+{
+	double sum = 0;
+
+	//sum prev layer's outputs
+	//include bias node
+
+	for (unsigned n = 0; n < prevLayer.size(); ++n)
+	{
+		sum += prevLayer[n].getOutputVal() * prevLayer[n].m_outputWeights[m_myIndex].weight;
+	}
+
+	m_outputVal = Neuron::transferFunction(sum);
+}
+
+void Neuron::calcOutputGradients(double targetVal)
+{
+	double delta = targetVal - m_outputVal;
+	m_gradient = delta * Neuron::transferFunctionDerivative(m_outputVal);
+}
+
+void Neuron::calcHiddenGradients(const Layer &nextLayer)
+{
+	double dow = sumDOW(nextLayer);
+	m_gradient = dow * Neuron::transferFunctionDerivative(m_outputVal);
+}
+
+
+class QNet
+{
+public:
+	QNet(const vector<unsigned> &togology);
+	void feedForward(const vector<double> &inputVals);
+	void backProp(const vector<double> &targetVals);
+	void getResults(vector<double> &resultVals) const;
+	double getRecentAverageError(void) const { return m_recentAverageError; }
+private:
+	vector<Layer> m_layers; // m_layers[layerNum][neuronNum]
+	double m_error;
+	double m_recentAverageError;
+	double m_recentAverageSmoothingFactor;
+};
+
+QNet::QNet(const vector<unsigned> &topology)
+{
+	unsigned numLayers = topology.size();
+	for (unsigned layerNum = 0; layerNum < numLayers; ++layerNum)
+	{
+		m_layers.push_back(Layer());
+		unsigned numOutputs = layerNum == topology.size() - 1 ? 0 : topology[layerNum + 1];
+		//added layer, fill it with ith neurons and also add bias neuron (<= in forloop)
+
+		for (unsigned neuronNum = 0; neuronNum <= topology[layerNum]; ++neuronNum)
+		{
+			m_layers.back().push_back(Neuron(numOutputs, neuronNum));
+			cout << "Neuron created" << endl;
+		}
+
+		//force the bias node's output value to 1.0
+		m_layers.back().back().setOutputVal(1.0);
+	}
+}
+
+void QNet::feedForward(const vector<double> &inputVals)
+{
+	//check number of inputs is same as number of input neurons
+	assert(inputVals.size() == m_layers[0].size() - 1);
+
+	//assign the input values into the input neurons
+	for (unsigned i = 0; i < inputVals.size(); ++i)
+	{
+		m_layers[0][i].setOutputVal(inputVals[i]);
+	}
+
+	//foward propagate
+	for (unsigned layerNum = 1; layerNum < m_layers.size(); ++layerNum)
+	{
+		Layer prevLayer = m_layers[layerNum - 1];
+		for (unsigned n = 0; n < m_layers[layerNum].size() - 1; ++n)
+		{
+			m_layers[layerNum][n].feedForward(prevLayer);
+		}
+	}
+
+}
+
+void QNet::backProp(const vector<double> &targetVals)
+{
+	// calc overall net error (RMS of output neuron error)
+	Layer &outputLayer = m_layers.back();
+	m_error = 0.0;
+
+	for (unsigned n = 0; n < outputLayer.size() - 1; ++n)
+	{
+		double delta = targetVals[n] - outputLayer[n].getOutputVal();
+		m_error += delta * delta;
+	}
+
+	m_error /= outputLayer.size() - 1;
+	m_error = sqrt(m_error);
+
+	// implement recent average measurement
+	m_recentAverageError = (m_recentAverageError * m_recentAverageSmoothingFactor + m_error) / (m_recentAverageSmoothingFactor + 1.0);
+
+	// calc output layer gradients
+	for (unsigned n = 0; n < outputLayer.size() - 1; ++n)
+	{
+		outputLayer[n].calcOutputGradients(targetVals[n]);
+	}
+
+	// calc gradients on hidden layers
+	for (unsigned layerNum = m_layers.size() - 2; layerNum > 0; --layerNum)
+	{
+		Layer &hiddenLayer = m_layers[layerNum];
+		Layer &nextLayer = m_layers[layerNum + 1];
+
+		for (unsigned n = 0; n < hiddenLayer.size(); ++n)
+		{
+			hiddenLayer[n].calcHiddenGradients(nextLayer);
+		}
+	}
+
+	// for all layers from outputs to first hidden layer, update connection weights
+	for (unsigned layerNum = m_layers.size() - 1; layerNum > 0; --layerNum)
+	{
+		Layer &layer = m_layers[layerNum];
+		Layer &prevLayer = m_layers[layerNum - 1];
+
+		for (unsigned n = 0; n < layer.size() - 1; ++n)
+		{
+			layer[n].updateInputWeights(prevLayer);
+		}
+	}
+
+}
+
+void QNet::getResults(vector<double> &resultVals) const
+{
+	resultVals.clear();
+
+	for (unsigned n = 0; n < m_layers.back().size() - 1; ++n)
+	{
+		resultVals.push_back(m_layers.back()[n].getOutputVal());
+	}
+}
+
+void showVectorVals(string label, vector<double> &v)
+{
+	cout << label << " ";
+	for (unsigned i = 0; i < v.size(); ++i) {
+		cout << v[i] << " ";
+	}
+
+	cout << endl;
+}
+
+
+int main()
+{
+	TrainingData trainData("trainingData.txt");
+
+	// e.g., { 3, 2, 1 }
+	vector<unsigned> topology;
+	trainData.getTopology(topology);
+
+	QNet myNet(topology);
+
+	vector<double> inputVals, targetVals, resultVals;
+	int trainingPass = 0;
+
+
+	while (!trainData.isEof()) {
+		++trainingPass;
+		cout << endl << "Pass " << trainingPass;
+
+		// Get new input data and feed it forward:
+		if (trainData.getNextInputs(inputVals) != topology[0]) {
+			break;
+		}
+		showVectorVals(": Inputs:", inputVals);
+		myNet.feedForward(inputVals);
+
+		// Collect the net's actual output results:
+		myNet.getResults(resultVals);
+		showVectorVals("Outputs:", resultVals);
+
+		// Train the net what the outputs should have been:
+		trainData.getTargetOutputs(targetVals);
+		showVectorVals("Targets:", targetVals);
+		assert(targetVals.size() == topology.back());
+
+		myNet.backProp(targetVals);
+
+		// Report how well the training is working, average over recent samples:
+		cout << "Net recent average error: "
+			<< myNet.getRecentAverageError() << endl;
+	}
+
+	cout << endl << "Training complete" << endl;
+
+	while (1)
+	{
+		inputVals.clear();
+		double in;
+		cin >> in;
+		inputVals.push_back(in);
+		cin >> in;
+		inputVals.push_back(in);
+
+		
+		showVectorVals(": Inputs:", inputVals);
+		myNet.feedForward(inputVals);
+		myNet.getResults(resultVals);
+		showVectorVals("Outputs:", resultVals);
+
+
+	}
+}
